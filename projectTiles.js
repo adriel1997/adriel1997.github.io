2d7fbbe9292badce5df838bf9968bd71923e513a
Adriealle Dsouza <adriealle.dsouza@gmail.com>
2021-09-12 15:43:32 +0530

    completed revamping of portfolio website

--- projectTiles.js
+++ projectTiles.js
@@ -1,23 +1,23 @@
 var projects = [
 {
 	"project_name":"Smart Parks(Android)",
-	"description":"An application which uses Firebase to allow users to book available parking slots in real time.It also provides the user a authentication option and uses Firebase and JSON to perform queries and store data.After booking confirmation a free spot fetched from the database in real - time and updated in the database with the relevant information.",
+	"description":"<b>My first project,</b> an application which manages users authentication and queries Firebase real time database to allow users to book available parking slots in real time.",
 	"website":"https://adriel1997.github.io/linkedProjects/smartparks/apppage.html",
 	"github":"https://github.com/adriel1997/SmartParks",
-	"image":"./images/projectPhotos/smartParksWeb.png",	
-	"order":"gal_a"
-},
-{
-	"project_name":"Smart Parks(Web)",
-	"description":"An online website which uses client side scripting languages like html, css and javascript and uses a real time database from Firebase.It hosts the download link for the android application of the same name.It also has a feature which shows you the real time database of car parks after user authentication which was integrated using Google Sign - in or Email Sign - in",
-	"website":"https://adriel1997.github.io/linkedProjects/smartparks/home.html",
-	"github":"https://github.com/adriel1997/adriel1997.github.io/tree/master/linkedProjects/smartparks",
 	"image":"./images/projectPhotos/smartParksApp.png",	
 	"order":"gal_a"
 },
 {
+	"project_name":"Smart Parks(Web)",
+	"description":"A website which uses html, css, javascript and uses a real time database from Firebase. It hosts the download link for Smart Parks(Android). It has a feature which shows you the real time status of car parks after user authentication.",
+	"website":"https://adriel1997.github.io/linkedProjects/smartparks/home.html",
+	"github":"https://github.com/adriel1997/adriel1997.github.io/tree/master/linkedProjects/smartparks",
+	"image":"./images/projectPhotos/smartParksWeb.png",	
+	"order":"gal_a"
+},
+{
 	"project_name":"Third Eye(Android)",
-	"description":"Developing an android application with another developer using Google Cloud, DialogFlow, Tensorflow and Firebase.",
+	"description":"Developing an android application with using Google Cloud, DialogFlow and Firebase.",
 	"website":null,
 	"github":"https://github.com/adriel1997/Major-Project/blob/master/Major%20Project%20Report.pdf",
 	"image":"./images/projectPhotos/thirdEyeApp.png",	
@@ -25,7 +25,7 @@ var projects = [
 },
 {
 	"project_name":"Twitter Analysis(Java)",
-	"description":" Twitter based sentiment analysis using JAVA and Hadoop.In this project we are doing the sentiment analysis on twitter data to analyse whether the tweets posted by people are positive or negative or neutral by checking the tweets with the AFFIN dictionary which has a set of 2500 words along with the value of each word ranging from - 5 to + 5 denoting whether tweets are positive or negative. ",
+	"description":" Twitter based sentiment analysis using JAVA, Flume and Hadoop to analyse whether the tweets posted by people are positive or negative or neutral by checking the tweets with the AFFIN dictionary which contains a score for each word.",
 	"website": null,
 	"github":"https://github.com/adriel1997/Twitter-Sentiment-Analysis#twitter-sentiment-analysis",
 	"image":"./images/projectPhotos/sentimentAnalysis.png",	
@@ -33,7 +33,7 @@ var projects = [
 },
 {
 	"project_name":"Jolt",
-	"description":"Jolt :Industrial project which provides solar chargable scooties to people of Delhi / NCR and college students, to commute to shorter distances at cheap fares. Developed an android application with 3 other developers and oversaw the development of the entire application.Was responsible for developing various components ranging from database integration and administration to user authentication and storage of sensitive documents on the server(for earlier versions of application).",
+	"description":"Jolt :Industrial project which provides solar chargable scooters to people of Delhi / NCR and college students, to commute to shorter distances at cheap fares. Oversaw the development of 3 android applications. Database integration, administration to user authentication and storage of sensitive documents and scooter finding algorithum were some of the key responsibilities.",
 	"website":"https://www.joltindia.com/",
 	"github":"https://github.com/adriel1997/jolt",
 	"image":"./images/projectPhotos/joltApp.png",	
@@ -57,7 +57,9 @@ document.addEventListener('DOMContentLoaded', function() {
 		img.className = "img-fluid testing1";
 		imgDiv.className = 'img-overlay ';
 		a.className = 'hoverbutton global-radius';
-		i.className = "fa fa-picture-o";
+		i.className = "fa fas fa-book";
+		i.setAttribute('data-toggle','modal');
+		i.setAttribute('data-target','#myModal');
 		i.onclick = function() {openModelWindow(project['project_name']);};
 		
 		a.appendChild(i);
@@ -71,10 +73,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function openModelWindow(project_name){
 	var project  = projects.find(function(project){ return project['project_name'] === project_name; });
-	document.getElementById('modal-window-heading').innerHTML = project['project_name'];
-	document.getElementById('modal-window-content').innerHTML = project['description'];
+	document.getElementsByClassName('modal-title')[0].innerHTML = project['project_name'];
+	document.getElementsByClassName('modal-description')[0].innerHTML = project['description'];
 	document.getElementById('github').href = project['github'];
 	document.getElementById('website').href = project['website'];
-	//document.getElementById('model-image').src = project['image'];
-	document.getElementById('id01').style.display='block';
+	document.getElementById('model-image').src = project['image'];
+	document.getElementById('myModal').style.display='block';
 }
